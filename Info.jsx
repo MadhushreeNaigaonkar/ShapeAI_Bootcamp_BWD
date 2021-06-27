4e0aaad93dfa7cbf12990153ea5d36536893cf9c
MadhushreeNaigaonkar <86558178+MadhushreeNaigaonkar@users.noreply.github.com>
2021-06-27 20:16:21 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Info() {
+  return (<div className="note">
+    <h1> Javascript and React.js</h1>
+    <p> A Basic Bootcamp that will introduce yout to a dynamic website </p></div>
+  );
+}
+export default Info;
